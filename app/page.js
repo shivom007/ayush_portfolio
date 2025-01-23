14e47ff073fb0433700fa03b52b2e930deef688c
shivom007 <shivmahto37@gmail.com>
2025-01-23 22:07:27 +0530

    fifth commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 "use client";
-import dynamic from "next/dynamic";
+
+import { useState, useEffect } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-// import Blog from "./components/homepage/blog";
-
-const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
-
+import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -13,33 +11,38 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+  const [error, setError] = useState(null);
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await res.json();
+        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+        setBlogs(filtered);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
 
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
-export default async function Home() {
-  const blogs = await getData();
+    fetchData();
+  }, []);
 
   return (
-    <div suppressHydrationWarning >
+    <div>
       <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      <Blog blogs={blogs} />
+      {error ? <p>Error fetching blogs: {error}</p> : <Blog blogs={blogs} />}
       <ContactSection />
     </div>
-  )
-};
+  );
+}
