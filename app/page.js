9da424ee0307c79235f73792f6de05e79916d435
shivom007 <shivmahto37@gmail.com>
2025-01-23 21:36:21 +0530

    fourth commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 "use client";
+import dynamic from "next/dynamic";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+// import Blog from "./components/homepage/blog";
+
+const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
+
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
