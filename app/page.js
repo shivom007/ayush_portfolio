53e56f990c01f3fe1130158197399f569fe7ceaa
shivom007 <shivmahto37@gmail.com>
2025-01-23 22:17:24 +0530

    sixth commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-"use client";
+
 
 import { useState, useEffect } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+// import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -15,23 +15,23 @@ export default function Home() {
   const [blogs, setBlogs] = useState([]);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
-        if (!res.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const data = await res.json();
-        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-        setBlogs(filtered);
-      } catch (err) {
-        setError(err.message);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
+  //       if (!res.ok) {
+  //         throw new Error("Failed to fetch data");
+  //       }
+  //       const data = await res.json();
+  //       const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+  //       setBlogs(filtered);
+  //     } catch (err) {
+  //       setError(err.message);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
     <div>
@@ -41,7 +41,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
-      {error ? <p>Error fetching blogs: {error}</p> : <Blog blogs={blogs} />}
+      {/* {error ? <p>Error fetching blogs: {error}</p> : <Blog blogs={blogs} />} */}
       <ContactSection />
     </div>
   );
